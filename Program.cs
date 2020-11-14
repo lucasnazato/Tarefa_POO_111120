4bea9b6a79377aeaee41dc09fd354a6f7455d5c4
Lucas Nazato <36552433+lucasnazato@users.noreply.github.com>
2020-11-14 08:13:29 -0300

    Commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace Tarefa_POO_111120
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Criar conta
+            ContaJuridica conta = new ContaJuridica(8010, "Lara Croft", 100.0, 500.0);
+
+            // Dados da conta
+            Console.WriteLine("Conta: " + conta.Numero);
+            Console.WriteLine("Titular: " + conta.Titular);
+            Console.WriteLine("Saldo: R$ " + conta.Saldo.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Emprestimo Limite: R$ " + conta.EmprestimoLimite.ToString("F2", CultureInfo.InvariantCulture));
+
+            // Realizar saque da conta
+            Console.WriteLine("\nSaque de R$ 50.00");
+            conta.Saque(50);
+            Console.WriteLine("Saldo: R$ " + conta.Saldo.ToString("F2", CultureInfo.InvariantCulture));
+
+            // Realizar deposito na conta
+            Console.WriteLine("\nDeposito de R$ 200.00");
+            conta.Deposito(200);
+            Console.WriteLine("Saldo: R$ " + conta.Saldo.ToString("F2", CultureInfo.InvariantCulture));
+
+            // Emprestimo acima do valor permitido
+            Console.WriteLine("\nEmprestimo de R$ 600.00");
+            conta.Emprestimo(600);
+            Console.WriteLine("Saldo: R$ " + conta.Saldo.ToString("F2", CultureInfo.InvariantCulture));
+
+            // Emprestimo abaixo do valor permitido
+            Console.WriteLine("\nEmprestimo de R$ 450.00");
+            conta.Emprestimo(450);
+            Console.WriteLine("Saldo: R$ " + conta.Saldo.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadKey();
+        }
+    }
+}
